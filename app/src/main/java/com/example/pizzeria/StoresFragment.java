911c76648cf5d66ad5b073992584dc417de2a57f
metua <imerei777@gmail.com>
2020-12-29 23:08:06 +0600

    Fourth commit

---
+++ app/src/main/java/com/example/pizzeria/StoresFragment.java
@@ -0,0 +1,27 @@
+package com.example.pizzeria;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+public class StoresFragment extends ListFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                inflater.getContext(),
+                android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.stores)
+        );
+        setListAdapter(arrayAdapter);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
